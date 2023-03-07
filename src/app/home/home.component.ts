0e28b8be18017c5d0bc04cad22a6b902dc1088eb
Jonathan Fernandez <67233843+jonathandevcom@users.noreply.github.com>
2023-03-07 08:08:03 +0100

    http Client - update home - create interface article

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  constructor(private _httpClient: HttpClient) {}
 
-  constructor() { }
-
+  article: Article[]|undefined;
+  
   ngOnInit(): void {
+/*
+    const headers = new HttpHeaders({'username-4D':'toto', 'password-4D':'toto'});
+    console.log(headers);
+    
+    this._httpClient.post('http://localhost/rest/$directory/login',{headers: headers}).subscribe((res) => {
+      console.log(res );
+    });
+*/
+
+this._httpClient.get('http://localhost/rest/User').subscribe((res) => {
+  console.log(res);
+});
+
+    this._httpClient.get('https://fakestoreapi.com/products').subscribe((res) => {
+    console.log(res); 
+
+    this.article=res as Article[];
+
+    });
+
   }
 
 }
---
+++ src/app/interfaces/article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    id?: string;
+    title?: string;
+    summary?: string;
+    unitPriceExcludingTaxes?: number;
+    unitPriceIncludingTaxes?: number;
+}
