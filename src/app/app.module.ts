5c238467fc139588e054145f3d2efbda4a832046
Jonathan Fernandez <67233843+jonathandevcom@users.noreply.github.com>
2023-04-17 12:06:16 +0200

    Template form register with error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { LegalMentionComponent } from './components/legal-mention/legal-mention.
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { UserAdministrationComponent } from './components/user-administration/user-administration.component';
 import { CardArticleComponent } from './components/card-article/card-article.component';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { CardArticleComponent } from './components/card-article/card-article.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      civility: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator()]],
+      confirmPassword: ['', [Validators.required, this.matchPasswordValidator()]],
+      termsCheckbox: ['', Validators.requiredTrue]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  passwordValidator(): ValidatorFn {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = control.value;
+      if (!passwordRegex.test(password)) {
+        return { passwordInvalid: true };
+      }
+      return null;
+    };
+  }
+
+  matchPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = control.parent?.get('password')?.value;
+      const confirmPassword = control.value;
+      if (password !== confirmPassword) {
+        return { passwordNotMatch: true };
+      }
+      return null;
+    };
   }
 
 }
