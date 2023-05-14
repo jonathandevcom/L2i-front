0c486f7317bc999e63f5623e53b2f35e365292cb
Jonathan Fernandez <67233843+jonathandevcom@users.noreply.github.com>
2023-05-14 10:41:01 +0200

    add errors message authorForm

--- src/app/components/admin-administration/author/author.component.ts
+++ src/app/components/admin-administration/author/author.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { AuthorService } from '../../../services/author.service';
 
 @Component({
@@ -11,13 +12,33 @@ export class AuthorComponent implements OnInit {
   authors!: any[]; // Tableau d'auteurs
   selectedAuthor: any = {}; // Auteur sélectionné initialisé avec un objet vide
   selectedAuthorCheck: boolean = false;
+  authorForm!: FormGroup;
+  submitted = false;
 
-  constructor(private authorService: AuthorService) { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private authorService: AuthorService
+  ) { }
 
   ngOnInit(): void {
     this.getAuthors();
+
+    this.authorForm = this.formBuilder.group({
+      id: [''], // Ajouter les autres champs ici avec leurs validations
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      language: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    let message: string = '';
+    this.submitted = true;
+    console.log(this.authorForm.invalid)
+    if (this.authorForm.invalid) {
+      return;
+    }
+  }
   getAuthors(): void {
     this.authorService.getAllAuthor().subscribe({
       next: (response: any) => {
@@ -56,7 +77,48 @@ export class AuthorComponent implements OnInit {
   }
 
   postAuthor() {
-    this.authorService.postAuthor(JSON.stringify(this.selectedAuthor).replace(/,/g, ';')).subscribe({
+    if (this.authorForm.invalid) {
+      return;
+    }
+
+    const formValues = this.authorForm.value;
+    const authorData = {
+      id: formValues.id,
+      firstname: formValues.firstname,
+      lastname: formValues.lastname,
+      language: formValues.language
+    };
+
+    this.authorService.postAuthor(JSON.stringify(authorData).replace(/,/g, ';')).subscribe({
+      next: (response: any) => {
+        this.handleResponse(response);
+        this.submitted = false;
+        this.authorForm.patchValue({
+          id: "",
+          firstname: "",
+          lastname: "",
+          language: ""
+        });
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+
+  updateAuthor() {
+    if (this.authorForm.invalid) {
+      return;
+    }
+
+    const formValues = this.authorForm.value;
+    const authorData = {
+      id: formValues.id,
+      firstname: formValues.firstname,
+      lastname: formValues.lastname,
+      language: formValues.language
+    };
+
+    this.authorService.putAuthor(this.selectedAuthor.ID, JSON.stringify(authorData).replace(/,/g, ';')).subscribe({
       next: (response: any) => {
         this.handleResponse(response);
       },
@@ -64,14 +126,6 @@ export class AuthorComponent implements OnInit {
     });
   }
 
-  updateAuthor() {
-    this.authorService.putAuthor(this.selectedAuthor.ID, JSON.stringify(this.selectedAuthor).replace(/,/g, ';')).subscribe({
-      next: (response: any) => {
-        this.handleResponse(response);
-      },
-      error: (error) => console.log(error),
-    });
-  }
 
   deleteAuthor(): void {
     this.authorService.deleteAuthor(this.selectedAuthor.ID).subscribe({
@@ -79,6 +133,13 @@ export class AuthorComponent implements OnInit {
         this.handleResponse(response);
         this.selectedAuthor = {};
         this.selectedAuthorCheck = false;
+        this.submitted = false;
+        this.authorForm.patchValue({
+          id: "",
+          firstname: "",
+          lastname: "",
+          language: ""
+        });
       },
       error: (error) => console.log(error),
     });
@@ -87,10 +148,28 @@ export class AuthorComponent implements OnInit {
   selectAuthor(author: any): void {
     this.selectedAuthor = author;
     this.selectedAuthorCheck = true;
+    this.submitted = false;
+
+    // Mettre à jour les valeurs du formulaire
+    this.authorForm.patchValue({
+      id: author.id,
+      firstname: author.firstname,
+      lastname: author.lastname,
+      language: author.language
+    });
   }
+
 
   addAuthor(): void {
     this.selectedAuthor = {};
     this.selectedAuthorCheck = false;
+    this.submitted = false;
+    // Mettre à jour les valeurs du formulaire
+    this.authorForm.patchValue({
+      id: "",
+      firstname: "",
+      lastname: "",
+      language: ""
+    });
   }
 }
